7d6c003c2151834887c4c5c3472a96690f7fcb07
Roberto Aguirre <agsaro7@gmail.com>
2022-06-28 18:27:25 -0500

    basename

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
   
-    <BrowserRouter>
+    <BrowserRouter basename='/hidropozo-vite/'>
       <Routes>
         <Route exact path="/" element={<Inicio/>}/>
         <Route exact path="/contacto" element={<Contacto/>}/>
